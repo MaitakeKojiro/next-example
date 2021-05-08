06828f983f4398f841ed1ffbe2491787175c611e
MaitakeKojiro <nopeysan526@gmail.com>
2021-05-08 19:01:57 +0900

    レッスン4.プリレンダリングとデータフェッチング

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout>
       <Head>
@@ -15,6 +16,33 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+// getStaticPorpsを使うことで外部からデータを取得できる。
+// getStaticPropsは毎回のリクエストごとに実行される
+export async function getStaticProps() {
+  // ファイルシステムや API、 DB などから外部データを取得する
+  const allPostsData = getSortedPostsData()
+  return {
+    // `props`キーに対応する値が `Home` コンポーネントに渡される
+    props: {
+      allPostsData
+    }
+  }
 }
