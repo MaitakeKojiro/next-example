cbc60f0646cb159cd10fbf011c469092d97ea336
MaitakeKojiro <nopeysan526@gmail.com>
2021-05-09 14:27:38 +0900

    レッスン8.TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,33 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Date from '../components/date'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Head from "next/head"
+import Layout, { siteTitle } from "../components/layout"
+import utilStyles from "../styles/util.module.css"
+import Link from "next/link"
+import Date from "../components/date"
+import { GetStaticProps } from "next"
+import { getSortedPostsData } from "../lib/posts"
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>舞茸小次郎です。Next.js勉強中です。</p>
         <p>
-          (このサイトを見て勉強しています。{' '}
+          (This is a sample website - you'll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>ブログ</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
@@ -38,15 +46,11 @@ export default function Home({ allPostsData }) {
   )
 }
 
-// getStaticPorpsを使うことで外部からデータを取得できる。
-// getStaticPropsは毎回のリクエストごとに実行される
-export async function getStaticProps() {
-  // ファイルシステムや API、 DB などから外部データを取得する
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
-    // `props`キーに対応する値が `Home` コンポーネントに渡される
     props: {
-      allPostsData
-    }
+      allPostsData,
+    },
   }
 }
